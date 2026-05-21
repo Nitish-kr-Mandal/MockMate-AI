0f268137d898848bda03afc030ef83356cfcfe2f
Nitish-kr-Mandal <nitishm8292@gmail.com>
2026-05-21 21:28:21 +0530

    setup user authentication

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/connectDb.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config()
 
 const app = express()
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
 
 const PORT = process.env.PORT || 6000
-
-app.get("/", (req, res) => {
-    return res.json({message:"server is running"})
-})
-
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)       
     connectDb()            
